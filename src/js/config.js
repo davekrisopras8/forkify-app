6726db0a98fb8038d68d41314e379bb5387cb5c2
Dave Krisopras Essanto <davemantul123456@gmail.com>
2025-03-17 12:04:27 +0700

    initial

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/'
+export const TIMEOUT_SEC = 10
+export const RES_PER_PAGE = 10
+export const APIKEY = 'cca50ab7-b844-4869-b305-633fe6df23f2'
+export const MODAL_CLOSE_SEC = 2.5
